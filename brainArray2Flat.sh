a32cb8ff6cb8eaf150d880c9287db99e88083699
Alessandro Pietrelli <cobraimovic@gmail.com>
2016-12-01 18:48:07 +0100

    rm tmp file at the end

--- brainArray2Flat.sh
+++ brainArray2Flat.sh
@@ -142,7 +142,7 @@ echo "[Generate Flat file] [`date +%c`] Join filtered probe/probeset with Descri
 join -t $'\t' <(awk 'BEGIN{FS="\t";OFS="\t"}{print $2,$0}' "$OUT"_probe_tab.probe_flt.probeset_flt | sort -k1,1) <(awk 'BEGIN{FS="\t";OFS="\t"}{split($2,anno,"|");print $1,anno[1]}' $DESC | sort -k1,1) > tmp
 echo "[Generate Flat file] Arrange Flat file for correct interpretation in flat2CDF.R"
 awk 'BEGIN{FS="\t";OFS="\t"}{if (NR==1){print "Probe_ID", "X", "Y", "Probe_Sequence", "Group_ID", "Unit_ID"};print $2,$4,$5,$7,$1,$9}' tmp > $OUT.flat
-#rm tmp
+rm tmp
 
 echo "FILTERING COMPLETE"
 
